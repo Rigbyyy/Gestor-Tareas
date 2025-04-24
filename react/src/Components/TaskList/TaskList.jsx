873bf8ca4361ef9043e04d0a3023dc5c64329ac3
Erik Warnke <erwarnkeva@est.utn.ac.cr>
2025-04-24 10:23:42 -0600

    TaskList Base
    Creacion Base del componente TaskList

--- react/src/Components/TaskList/TaskList.jsx
+++ react/src/Components/TaskList/TaskList.jsx
@@ -1,10 +1,20 @@
-function TaskList () {
-  const [count, setCount] = useState(0)
 
+function TaskList ({tasks}) {
+  
+  const listItems = tasks.map(task =>
+    <li
+      key={task.id}
+      style={{
+        color: task.isComplete ? 'green' : 'red'
+      }}
+    >
+      {task.text}
+    </li>
+  );
   return (
-   <>
-   </>
-  )
+    <ul>{listItems}</ul>
+  );
+
 }
 
 export default TaskList;
