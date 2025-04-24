f15e9208de5c75938c972188e27ae72c4059f016
Rigbyyy <cazunigaca@est.utn.ac.cr>
2025-04-24 10:34:09 -0600

    Creacion Checkbox correctamente
    Cree el checkbox donde no era

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -5,15 +5,7 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TaskList from './Components/TaskList/TaskList';
 function App() {
- /* const [count, setCount] = useState(0)
-<div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>*/
+ 
 
   return (
     
@@ -26,7 +18,7 @@ function App() {
       type="text"
       className="form-control"
       id="InputTarea"
-      aria-describedby="emailHelp"
+      aria-describedby="textHelp"
     />
     
   </div>
--- react/src/Components/TaskList/TaskList.jsx
+++ react/src/Components/TaskList/TaskList.jsx
@@ -3,13 +3,9 @@ function TaskList () {
 
   return (
 
+  <>
   
-   <div className="mb-3 form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-    <label className="form-check-label" htmlFor="exampleCheck1">
-      Completado
-    </label>
-   </div>
+   </>
   )
 }
 
