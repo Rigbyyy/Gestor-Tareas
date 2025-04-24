a776ec6b203d73dbe7fe48209d5fbace5ed70406
Rigbyyy <cazunigaca@est.utn.ac.cr>
2025-04-24 10:07:54 -0600

    Creación de un input en tarea en App
    Creé un input para ingreasar el nombre de la tarea, ademas de crear el checkbox en TaskList

--- react/src/Components/TaskList/TaskList.jsx
+++ react/src/Components/TaskList/TaskList.jsx
@@ -2,8 +2,14 @@ function TaskList () {
   const [count, setCount] = useState(0)
 
   return (
-   <>
-   </>
+
+  
+   <div className="mb-3 form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+    <label className="form-check-label" htmlFor="exampleCheck1">
+      Completado
+    </label>
+   </div>
   )
 }
 
